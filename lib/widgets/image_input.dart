a323937335a36889d7e112ea8d95f8bc7b3c85fc
TomekLesniak <tomasz.lesniak152@gmail.com>
2020-10-30 12:48:03 +0100

    Taking a picture functionality

---
+++ lib/widgets/image_input.dart
@@ -0,0 +1,58 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImageInput extends StatefulWidget {
+  @override
+  _ImageInputState createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  File _storedImage;
+  final imagePicker = ImagePicker();
+
+  Future<void> _takePicture() async {
+    final pickedImage =
+        await imagePicker.getImage(source: ImageSource.camera, maxWidth: 600);
+
+    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: 150,
+          height: 100,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.grey),
+          ),
+          child: _storedImage != null
+              ? Image.file(
+                  _storedImage,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                )
+              : Text(
+                  'No Image Chosen',
+                  textAlign: TextAlign.center,
+                ),
+          alignment: Alignment.center,
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        Expanded(
+          child: FlatButton.icon(
+            icon: Icon(Icons.camera),
+            textColor: Theme.of(context).primaryColor,
+            label: Text('Choose picture'),
+            onPressed: _takePicture,
+          ),
+        ),
+      ],
+    );
+  }
+}
